37404e0d07560f2c81605d1ccf6b2913f35ef165
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2024-09-22 20:49:31 +0500

    this is now done

--- src/components/nevbar.tsx
+++ src/components/nevbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className='w-full flex items-center justify-between max-w-4xl mx-auto px-4 py-5'>
       <Link href="/" className='font-bold text-3xl'>
       Torque<span className='text-primary'>&</span>
-      <span className='flex'>Triumph</span>
+      <span>Triumph</span>
       </Link>
       <div className='ml-auto'>
         <ModeToggle />
